64aa622c044668377d7a2c7a815f174d90abab75
EncryptEx <limusina10@gmail.com>
2023-03-12 00:53:03 +0100

    Finished info and created dark mode

--- src/index.php
+++ src/index.php
@@ -37,8 +37,7 @@ function monthToString($monthNum)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/df57820da4.js" crossorigin="anonymous"></script>
     <title>MyEvents - Jaume López Molina</title>
     <style>
@@ -51,15 +50,30 @@ function monthToString($monthNum)
             width: 100%;
             height: auto;
         }
+
+        #lightSwitch {
+            transform: scale(1.8);
+        }
     </style>
+    <script src="./static/js/toggler.js" defer></script>
 </head>
 
 <body>
     <div class="container mt-3 mt-md-5">
         <h1>Congresses and Hackathons</h1>
-
+        <!-- switch starts -->
+        <div class="d-flex">
+            <div class="form-check form-switch ms-auto mt-3 me-3">
+                <label class="form-check-label ms-3" for="lightSwitch">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
+                        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                    </svg>
+                </label>
+                <input class="form-check-input" type="checkbox" id="lightSwitch" />
+            </div>
+        </div>
+        <!-- switch ends -->
         <h3>Interactive Map</h3>
-
         <div class="row">
             <div class="col-md-6 col-12">
                 <h4>Hackathons Organized</h4>
@@ -73,7 +87,7 @@ function monthToString($monthNum)
                         <div class='col-md-11 col-12 mt-3'>
                             <div class="row mb-4 border rounded shadow p-3">
                                 <div class="col-4">
-                                    <img src="https://fakeimg.pl/250x250/" class="portait rounded">
+                                    <img src="<?php echo (htmlentities($hackathon['photo_url'])); ?>" class="portait rounded">
                                 </div>
                                 <div class="col-8">
                                     <div class="row">
@@ -120,7 +134,7 @@ function monthToString($monthNum)
                     <h4 style="margin-top:60px;">Hackathons Attended</h4>
                     <div class="row">
                         <?php
-                        $organized = retrieve("./data/organized_hackathons.json", TRUE);
+                        $organized = retrieve("./data/attended_hackathons.json", TRUE);
                         foreach ($organized as $hackathon) :
 
                         ?>
@@ -128,7 +142,7 @@ function monthToString($monthNum)
                             <div class='col-md-11 col-12 mt-3'>
                                 <div class="row mb-4 border rounded shadow p-3">
                                     <div class="col-4">
-                                        <img src="https://fakeimg.pl/250x250/" class="portait rounded">
+                                        <img src="<?php echo (htmlentities($hackathon['photo_url'])); ?>" class="portait rounded">
                                     </div>
                                     <div class="col-8">
                                         <div class="row">
@@ -186,7 +200,7 @@ function monthToString($monthNum)
                         <div class='col-md-11 col-12 mt-3'>
                             <div class="row mb-4 border rounded shadow p-3">
                                 <div class="col-4">
-                                    <img src="<?php echo(htmlentities($hackathon['photo_url'])); ?>" class="portait rounded">
+                                    <img src="<?php echo (htmlentities($hackathon['photo_url'])); ?>" class="portait rounded">
                                 </div>
                                 <div class="col-8">
                                     <div class="row">
