7dcbe4c0b18a8fd19a32a05c184c06d9434e33e5
EncryptEx <limusina10@gmail.com>
2023-03-20 10:11:09 +0100

    added mapbox

--- src/index.php
+++ src/index.php
@@ -39,6 +39,8 @@ function monthToString($monthNum)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/df57820da4.js" crossorigin="anonymous"></script>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
     <title>MyEvents - Jaume López Molina</title>
     <style>
         .icon {
@@ -62,7 +64,7 @@ function monthToString($monthNum)
     <div class="container mt-3 mt-md-5">
         <h1>Congresses and Hackathons</h1>
         <!-- switch starts -->
-        <div class="d-flex">
+        <!-- <div class="d-flex">
             <div class="form-check form-switch ms-auto mt-3 me-3">
                 <label class="form-check-label ms-3" for="lightSwitch">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
@@ -71,9 +73,21 @@ function monthToString($monthNum)
                 </label>
                 <input class="form-check-input" type="checkbox" id="lightSwitch" />
             </div>
-        </div>
+        </div> -->
         <!-- switch ends -->
         <h3>Interactive Map</h3>
+        <div id='map' style='width: 100%; height: auto; min-height:500px;'></div>
+        <hr class="mb-4">
+        <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZW5jcnlwdGV4IiwiYSI6ImNsZjVuMm54NzBtbHYzd3FoZ3h6czh1MWIifQ.a-ffpy_yiO84rbT774vpaw';
+            var map = new mapboxgl.Map({
+                container: 'map',
+                // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [41, -3],
+                zoom: 3
+            });
+        </script>
         <div class="row">
             <div class="col-md-6 col-12">
                 <h4>Hackathons Organized</h4>
